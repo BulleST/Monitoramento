2dfc05cea3d88f5c240ec057a4031b49aef9bdb7
Noemi1 <calmeida.no@gmail.com>
2023-02-17 18:25:47 -0300

    modulos

---
+++ src/app/pages/rpas/rpas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RpasRoutingModule } from './rpas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RpasRoutingModule
+  ]
+})
+export class RpasModule { }
---
+++ src/app/pages/webservices/webservices.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WebservicesRoutingModule } from './webservices-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WebservicesRoutingModule
+  ]
+})
+export class WebservicesModule { }
